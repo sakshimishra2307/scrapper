9db29809403db9a958b54b50820371241555add9
sakshimishra2307 <38456741+sakshimishra2307@users.noreply.github.com>
2023-08-03 17:07:18 +0530

    Update and rename yelp.js to index.js

--- handlers/yelp/index.js
+++ handlers/yelp/index.js
@@ -3,26 +3,27 @@
 const puppeteer = require("puppeteer");
 const utils = require("./utils");
 const filterHelper = require("../../helpers/filterReviewsWithDate");
+const paginationFilter = require("../../helpers/pagination");
 
 /**
  * Yelp scrapper
- * @param {object} pageDetails http request parameters
+ * @param {object} req http request parameters
  * @param {object} res http response
  * @param {function} next invokes the succeeding middleware/function
  */
 
-const scrapeYelp = async (pageDetails, res, next) => {
+const scrapeYelp = async (req, res, next) => {
   try {
     console.log("processing for yelp data..");
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page
-      .goto(pageDetails.url, { waitUntil: "domcontentloaded" })
-      .catch((err) => {
-        console.log(`error while goto: ${err}`);
-      });
+    await page.goto(req.url, { waitUntil: "domcontentloaded" }).catch((err) => {
+      console.log(`error while goto: ${err}`);
+    });
     let reviews = await utils.fetchData(page, 0);
     let allReviews = await utils.processingReviews(page, reviews);
+    let pageSize = req.page_size || allReviews.length;
+    let pageNum = req.page_no || 1;
     await browser.close();
     console.log("page closed..");
     let result = allReviews.map(({ user, comment, localizedDate, rating }) => {
@@ -34,18 +35,18 @@ const scrapeYelp = async (pageDetails, res, next) => {
       };
     });
     result = filterHelper.sortReviews(result);
-    let filteredResult;
-    if (pageDetails.filter_date) {
-      filteredResult = filterHelper.filterReviews(
-        result,
-        pageDetails.filter_date
-      );
+    if (req.filter_date) {
+      result = filterHelper.filterReviews(result, req.filter_date);
     }
+    if ((pageNum - 1) * pageSize > result.length) {
+      return res.status(400).send({
+        message: "please provide valid page number and page size",
+      });
+    }
+    result = paginationFilter.paginationFilter(pageSize, pageNum, result);
     return {
-      review_count: pageDetails.filter_date
-        ? filteredResult.length
-        : result.length,
-      aggregated_reviews: pageDetails.filter_date ? filteredResult : result,
+      review_count: result.length,
+      aggregated_reviews: result,
     };
   } catch (err) {
     next(err);
